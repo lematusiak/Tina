676abd72b59dfa0f981e8e4f910686f748229408
lematusiak <leematusiak@gmail.com>
2023-11-09 14:32:53 +0100

    Tablice (09.11.2023)

--- Tina/Tina/Program.cs
+++ Tina/Tina/Program.cs
@@ -1,45 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿ 
+// TABLICE (09.11.2023)
 
-// INSTRUKCJA IF, ELSE (2023.11.05)
+// Tablice mają określoną wielkość. 
+// Tutaj będzie określona na 28 pór dnia
+// Czyli 1 tydzień, 7 dni x 4 pory dnia = 28
 
-// ELSE występuje w sytuacji, gdy sprawdzany warunek IF jest fałszywy
-// Dwie instrukcje if i else można ze sobą łączyć
-// Robimy to dodając kolejnego ifa ... Czyli będzie to wyglądać else if
-// Program będzie sprawdzał do najbliższego elsa
-
-// int number1 = 50;
-// int number2 = 10;
-
-var name = "Adam";
-var age = 33;
-
-if (name != "Adam" || age > 40 && !(age > 40))
+namespace Lekcja5
 {
-    Console.WriteLine("Jestem Adamem");
+class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[28];
+            string[] timeOfDay = new string[4];
+
+// TABLICE TWORZYMY ZA POMOCĄ POWYŻSZEGO SZABLONU 
+// (dwa akapity powyżej)
+
+// ODWOŁUJEMY SIĘ DO KAŻDEJ KOMÓRKI Z OSOBNA (1 SPOSÓB)
+// (4 akapity poniżej)
+
+            timeOfDay[0] = "ranek";
+            timeOfDay[1] = "południe";
+            timeOfDay[2] = "wieczór";
+            timeOfDay[3] = "noc";
+
+            Console.WriteLine(timeOfDay[3]); // wyskoczy - noc
+            
+            
+            // (DRUGI) SPOSÓB
+            // wg szablonu poniżej: 
+
+            string[] timeOfDay2 = {"ranek", "południe", "wieczór", "noc" };
+
+            Console.WriteLine(timeOfDay2[1]);
+
+            Console.ReadKey();  // wyskoczy - południe 
+                                // czyli razem - noc, południe
+
+
+        }
+
+    }
+
 }
-else if (name != "Adam")
-{
-    Console.WriteLine("A może teraz to Adam");
-}
-else
-{
-    Console.WriteLine("Szukamy dalej Adama"); // ten tekst się wyświetli
-}
 
-Console.ReadKey();
 
-// OPERATORY RELACYJNE
-// 
-// == równa się
-// != jest różne
-// >  jest większe
-// <  jest mniejsze
-// >= większe lub równe
-// <= mniejsze lub równe
 
-// OPERATORY LOGICZNE
-
-// && i             (muszą być spełnione oba warunki)
-// II lub     or    (musi być spełniony przynajmniej 1 warunek)
-// ! negacja  not   (powyższa wartość jest zanegowana)
