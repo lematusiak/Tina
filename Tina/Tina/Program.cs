cf2175295a95702a3722f573db95cdb9007a4004
lematusiak <leematusiak@gmail.com>
2023-11-11 13:15:04 +0100

    Pętla For Wprowadzenie

--- Tina/Tina/Program.cs
+++ Tina/Tina/Program.cs
@@ -1,9 +1,14 @@
 ﻿ 
-// TABLICE (09.11.2023)
-
-// Tablice mają określoną wielkość. 
-// Tutaj będzie określona na 28 pór dnia
-// Czyli 1 tydzień, 7 dni x 4 pory dnia = 28
+//  PĘTLA FOR WPROWADZENIE (KURS IT, LEKCJA NR 5)
+//
+// Pozwala przez dany fragment przejść wielokrotnie. Składa się z 3 części:
+// 1) INICJALIZACJA, to zmienna (int, var, itd), która jest wartością, wskaźnikiem początkowym.
+// 2) WARUNEK, sprawdza czy pętla powinna być wykonana
+// Jeśli jest prawdziwy - pętla zostanie uruchomiona
+// Jeśli jest fałszywy - pętla zostanie zakończona
+// 3) ITERACJA, wyrażenie wykonywane na końcu pętli.
+// Inkrementuje lub dekrementuje (zwiększa lub zmniejsza wartość o 1)
+// zmienną sterującą aby zapewnić odpowiednie działanie pętli
 
 namespace Lekcja5
 {
@@ -14,28 +19,28 @@ class Program
             int[] grades = new int[28];
             string[] timeOfDay = new string[4];
 
-// TABLICE TWORZYMY ZA POMOCĄ POWYŻSZEGO SZABLONU 
-// (dwa akapity powyżej)
-
-// ODWOŁUJEMY SIĘ DO KAŻDEJ KOMÓRKI Z OSOBNA (1 SPOSÓB)
-// (4 akapity poniżej)
-
             timeOfDay[0] = "ranek";
             timeOfDay[1] = "południe";
             timeOfDay[2] = "wieczór";
             timeOfDay[3] = "noc";
 
-            Console.WriteLine(timeOfDay[3]); // wyskoczy - noc
-            
-            
-            // (DRUGI) SPOSÓB
-            // wg szablonu poniżej: 
+            // for składa się z trzech slotów, (trzech przestrzeni)
+            // w tym przypadku wyświetli się 0 - wartość początkowa.
+            // 1,2 i na końcu 3 jako wartość końcowa, zamykająca pętlę 
 
-            string[] timeOfDay2 = {"ranek", "południe", "wieczór", "noc" };
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(i);
+            }
 
-            Console.WriteLine(timeOfDay2[1]);
+           // Console.WriteLine(timeOfDay[3]); // wyskoczy - noc
+ 
 
-            Console.ReadKey();  // wyskoczy - południe 
+           // string[] timeOfDay2 = {"ranek", "południe", "wieczór", "noc" };
+
+           // Console.WriteLine(timeOfDay2[1]);
+
+           // Console.ReadKey();  // wyskoczy - południe 
                                 // czyli razem - noc, południe
 
 
